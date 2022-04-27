321365dbcafddf5f6860281944bab22d154f4ff4
Scott Miller <samiller@senomedical.com>
2022-04-27 15:03:05 -0500

    two building options

--- seno-build.sh
+++ seno-build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mvn install -Dmaven.test.skip -Dchangelist=-seno-`git rev-parse --short HEAD`
+# for debugging with IntelliJ
+# mvn install -Dmaven.test.skip -Dchangelist=-seno-`git rev-parse --short HEAD`
+
+# for packaging
+mvn -P compressXZ -Dportable=true -Dchangelist=-seno-`git rev-parse --short HEAD` -f weasis-distributions clean package
