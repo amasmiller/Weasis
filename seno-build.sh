9c9a0af20c3136511709a9b3cf276145eb74d0b5
Scott Miller <samiller@senomedical.com>
2022-05-06 15:58:41 -0500

    attempt to get build to work

--- seno-build.sh
+++ seno-build.sh
@@ -4,4 +4,6 @@
 mvn install -Dmaven.test.skip -Dchangelist=-seno-`git rev-parse --short HEAD`
 
 # for packaging
-#mvn -P compressXZ -Dportable=true -Dchangelist=-seno-`git rev-parse --short HEAD` -f weasis-distributions clean package
+mvn -P compressXZ -Dportable=true -Dchangelist=-seno-`git rev-parse --short HEAD` -f weasis-distributions clean package
+
+mvn -P compressXZ -Dchangelist=-seno-`git rev-parse --short HEAD` -f weasis-distributions clean package
